d408c6a24eb69e5f353ff9ff9d961f5146a0891a
Syrym-Joli <syrym.joli@gmail.com>
2024-10-11 22:56:45 +0500

    11

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
       </div>
-      <h1>Vite + React</h1>
+      <h1>Tarot 🃏</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        tg: @Syrym
       </p>
     </>
   )
